6cf8950ba9e5fbe3c9559bcab825faf0c9d7e83c
Geta Bugnar <bugnargeta@gmail.com>
2021-02-09 12:49:01 -0800

    added Database Configuration

---
+++ src/main/java/org/example/persistance/DatabaseConfiguration.java
@@ -0,0 +1,30 @@
+package org.example.persistance;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConfiguration {
+
+    public static Connection getConnection() throws SQLException, IOException {
+
+        InputStream dbproperties = DatabaseConfiguration.class.getClassLoader()
+                .getResourceAsStream("db.properties");
+
+        try {
+            Properties properties = new Properties();
+            properties.load(dbproperties);
+
+            return DriverManager.getConnection(properties.getProperty("url"),
+                    properties.getProperty("username"), properties.getProperty("password"));
+        } finally {
+            if (dbproperties != null) {
+                dbproperties.close();
+            }
+
+        }
+    }
+}
